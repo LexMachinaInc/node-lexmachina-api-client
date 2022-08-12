00280af2fda846e5ead64c65dd6b504145f140b3
Dave Slusher <dslusher@lexmachina.com>
2022-08-12 09:36:06 -0400

    Adding index.js as the entry point for exports

---
+++ index.js
@@ -0,0 +1,10 @@
+const LexMachinaClient = require("./src/lexmachina_client.js");
+const CasesQueryRequest = require("./src/case_query_request.js");
+const QueryDistrictCases = require("./src/query_district_cases.js");
+
+
+module.exports = {
+    LexMachinaClient,
+    CasesQueryRequest,
+    QueryDistrictCases
+}
