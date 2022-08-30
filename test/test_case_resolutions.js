1801ab618caa4aba9c5207d48d81ef2c1b3ca9c7
Dave Slusher <dslusher@lexmachina.com>
2022-08-30 14:18:09 -0400

    list-case-resolutions endpoint passes unit tests

--- test/test_case_resolutions.js
+++ test/test_case_resolutions.js
@@ -18,9 +18,9 @@ describe('List Case Resolutions', () => {
         nockDone();
         expect(caseResolutions).to.have.length.above(20);
 
-        caseResolutions.should.include("Claimant Win - Jury",);
-        caseResolutions.should.include("Procedural - Dismissal");
-        caseResolutions.should.include("Claim Defendant Win - Jury");
+        caseResolutions.should.deep.include({summary:"Claimant Win", specific:"Bench Trial"});
+        caseResolutions.should.deep.include({summary:"Procedural", specific:"Dismissal"});
+        caseResolutions.should.deep.include({summary:"Claim Defendant Win", specific:"Jury"});
 
     })
 })
